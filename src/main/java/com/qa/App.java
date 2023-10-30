0244579638621de8ae39da7879bcd571e256e9fb
Luke H <purelyfortoontown2@gmail.com>
2023-10-30 15:58:32 +0000

    addd luke name

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,6 +13,9 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
+
+        System.out.println(sayHelloToSomeone("LUKE!"));
+
         System.out.println(sayGoodbye());
     }
 
